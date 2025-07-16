781b93475ca8c65f358cfc20a81bf5fc5749f72a
IlijaIlic <ilic.ilija002@gmail.com>
2025-07-16 11:24:29 +0200

    Inital commit

---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_KnightMovement_FILE C:/Users/ilici/Desktop/GitRepos/KnightMovement/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/KnightMovement.exe)
+set(__QT_DEPLOY_TARGET_KnightMovement_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_KnightMovement_RUNTIME_DLLS C:/Users/ilici/Documents/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Users/ilici/Documents/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Users/ilici/Documents/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
